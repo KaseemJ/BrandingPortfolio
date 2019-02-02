9f584832a38b96ca567cec030b35e461b02691c9
wesley9mm <info.developer4@gmail.com>
2019-02-01 22:35:19 -0800

    add

--- js/app.js
+++ js/app.js
@@ -9,47 +9,12 @@ menuBtn[0].addEventListener('click', clickedBtn);
 console.log(menuBtn[0]);
 
 
-// $('a').on('click', function (e) {
-//     var targetSec = $(this).text();
-//     $('html, body').animate({
-//         scrollTop: $('#' + targetSec).offset().top
-//     }, 2000);
-// });
-
-
-
-$(document).ready(function() {
-    $('a[href*=#]').bind('click', function(e) {
-            e.preventDefault(); // prevent hard jump, the default behavior
-
-            var target = $(this).attr("href"); // Set the target as variable
-
-            // perform animated scrolling by getting top-position of target-element and set it as scroll target
-            $('html, body').stop().animate({
-                    scrollTop: $(target).offset().top
-            }, 600, function() {
-                    location.hash = target; //attach the hash (#jumptarget) to the pageurl
-            });
-
-            return false;
-    });
+$('a[href*=#]').on('click', function (e) {
+    var targetSec = $(this).text();
+    $('html, body').animate({
+        scrollTop: $('#' + targetSec).offset().top
+    }, 2000);
 });
 
-$(window).scroll(function() {
-    var scrollDistance = $(window).scrollTop();
 
-    // Show/hide menu on scroll
-    //if (scrollDistance >= 850) {
-    //		$('nav').fadeIn("fast");
-    //} else {
-    //		$('nav').fadeOut("fast");
-    //}
 
-    // Assign active class to nav links while scolling
-    $('.page-section').each(function(i) {
-            if ($(this).position().top <= scrollDistance) {
-                    $('.navigation a.active').removeClass('active');
-                    $('.navigation a').eq(i).addClass('active');
-            }
-    });
-}).scroll();
