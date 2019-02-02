b0674156310b7e072d5100d23d6a1b31017a2ede
wesley9mm <info.developer4@gmail.com>
2019-02-01 22:09:48 -0800

    add

--- js/app.js
+++ js/app.js
@@ -7,3 +7,49 @@ var clickedBtn = function(){
 menuBtn[0].addEventListener('click', clickedBtn);
 
 console.log(menuBtn[0]);
+
+
+// $('a').on('click', function (e) {
+//     var targetSec = $(this).text();
+//     $('html, body').animate({
+//         scrollTop: $('#' + targetSec).offset().top
+//     }, 2000);
+// });
+
+
+
+$(document).ready(function() {
+    $('a[href*=#]').bind('click', function(e) {
+            e.preventDefault(); // prevent hard jump, the default behavior
+
+            var target = $(this).attr("href"); // Set the target as variable
+
+            // perform animated scrolling by getting top-position of target-element and set it as scroll target
+            $('html, body').stop().animate({
+                    scrollTop: $(target).offset().top
+            }, 600, function() {
+                    location.hash = target; //attach the hash (#jumptarget) to the pageurl
+            });
+
+            return false;
+    });
+});
+
+$(window).scroll(function() {
+    var scrollDistance = $(window).scrollTop();
+
+    // Show/hide menu on scroll
+    //if (scrollDistance >= 850) {
+    //		$('nav').fadeIn("fast");
+    //} else {
+    //		$('nav').fadeOut("fast");
+    //}
+
+    // Assign active class to nav links while scolling
+    $('.page-section').each(function(i) {
+            if ($(this).position().top <= scrollDistance) {
+                    $('.navigation a.active').removeClass('active');
+                    $('.navigation a').eq(i).addClass('active');
+            }
+    });
+}).scroll();
